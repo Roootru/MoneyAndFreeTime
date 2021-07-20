e715c839473ef0e28305cf67b54cc12e6c22482c
Roootru <cnek1996@gmail.com>
2021-07-20 10:30:06 +0900

    Add a code writing/reading food info in/out file.

--- Food.h
+++ Food.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <string>
+class UserInterface;
 
 class Food
 {
 public:
+	Food();
 	Food(std::string n);
 	Food(std::string n, std::string desc);
 	Food(std::string n, float pr);
 	Food(std::string n, std::string desc, float pr);
+	void setdata(std::string nm, std::string desc, float pr);
+	friend class UserInterface;
 private:
 	std::string name;
 	std::string description;
--- UserInterface.cpp
+++ UserInterface.cpp
@@ -19,16 +19,32 @@ void UserInterface::ShowFoodSelectionWindow()
 {
     using namespace std;
     int CountOfFood = 0, SumOfOrder = 0;
+    fstream file; // создать входной/выходной файл
+    Food food; // создать объект person
 
     cout << endl;
     cout << endl;
     cout << "\t\t FOOD SELECTION MENU \n";
     cout << endl;
+
+    file.open("FOOD.DAT", ios::app | ios::out |
+        ios::in | ios::binary);
+
     cout << "\t BUCKET: \n";
     cout << "\t COUNT OF FOOD: " << CountOfFood << " \n";
     cout << "\t SUM OF ORDER: " << SumOfOrder << " \n";
     cout << "-----------------------------------------------------------------";
     // This is place where be food list, need function that get foor list
+    file.seekg(0); // поставить указатель на начало файла
+    // считать данные о первом человеке
+    file.read(reinterpret_cast<char*>(&food), sizeof(food));
+    while (!file.eof()) // Выход по EOF
+    {
+        cout << setw(10) << "NAME" << "DESCRIPTION" << "PRICE";// вывести данные
+        cout << setw(10) << food.name << food.description << food.price;
+        file.read(reinterpret_cast<char*>(&food), sizeof(food));
+    }
+    cout << endl;
     cout << "-----------------------------------------------------------------";
     cout << endl;
     cout << "\t\t PLEASE ENTER A FOOD NUMBER FROM LIST: ";
@@ -36,4 +52,33 @@ void UserInterface::ShowFoodSelectionWindow()
 
 void UserInterface::EnterDescriptionOfFood()
 {
+    using namespace std;
+
+    fstream file; // создать входной/выходной файл
+    Food food; // создать объект person
+    string name, description;
+    float price; // переменные для создания информации о еде
+    char ch; // считывает все ли мы заполнили или нет
+
+    cout << endl;
+    cout << endl;
+    cout << "\t\t FOOD INPUT MENU \n";
+    cout << endl;
+
+    // открыть для дозаписи
+    file.open("FOOD.DAT", ios::app | ios::out |
+        ios::in | ios::binary);
+    do // данные от пользователя - в файл
+    {
+        cout << "\t\t PLEASE ENTER AN INFORMATION ABOUT FOOD";
+        cout << "\n\t\t NAME: ";           cin >> name;
+        cout << "\n\t\t DESCRIPTION: ";    cin >> description;
+        cout << "\n\t\t PRICE: ";          cin >> price;
+        food.setdata(name, description, price);
+        // записать их в файл
+        file.write(reinterpret_cast<char*>(&food), sizeof(food));
+        cout << "Continue typing (y/n)? ";
+        cin >> ch;
+    } while (ch == 'y'); // выход по 'n'
+
 }
